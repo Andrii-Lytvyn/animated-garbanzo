d7f43266d20337eab1f84c72dbe925ab26f06955
AlexandrStopkin <stnalex1@gmail.com>
2023-03-28 21:42:25 +0200

    Add Classes, tz, resource files

---
+++ src/MenuTui.java
@@ -0,0 +1,21 @@
+package src;
+//line 64 - 76
+
+
+public class MenuTui {
+
+//  AllOutputs Graph + Logic
+//  Add Methods:
+
+//  ShowLogin -- Andrii Litvin
+//  ShowAll -- Sasha
+//  ShowMenu -- Andrii Golik, General boolean - true; user - false
+//  AddTask  -- Sasha
+//  ReadTask -- Sasha
+//  EditTask -- Sasha
+//  Gant -- Sasha
+//  FinishTask -- Sasha
+//  Delete -- Sasha
+//  ChangeUser -- Andrii Golik
+//  Exit   -- Andrii Golik!!!
+}
---
+++ src/Task.java
@@ -0,0 +1,7 @@
+package src;
+//TODO Andrey Litvin
+//fields ...
+// line 43 - 60
+public class Task {
+
+}
