f63203c84f2880fbb617962eb02a3f3e0bc0f3b7
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-29 14:18:40 +0200

    Add tryCatch for files. Add tasks in Tasks.txt

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 import src.Task;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    File inputFile = new File("src/rsc/Tasks.txt");
-    Task task = new Task();
-    task.parseTaskFromFile(inputFile);
+    try {
+      File inputFile = new File("src/rsc/Tasks.txt");
+      Task task = new Task();
+      task.parseTaskFromFile(inputFile);
+    } catch (FileNotFoundException e) {
+      System.err.println("File not found: " + e.getMessage());
+    }catch (IOException e){
+      System.err.println("Input/output exception: " + e.getMessage());
+    }
 
   }
 }
