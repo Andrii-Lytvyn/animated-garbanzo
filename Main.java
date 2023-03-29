630576b73c210ebd73ca371f3f5699e72c66f468
AlexandrStopkin <stnalex1@gmail.com>
2023-03-29 15:08:26 +0200

    Add List<Task>

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-  List<Task> tasks = new ArrayList<>();
+  public List<Task> tasks = new ArrayList<>();
   public static void main(String[] args) throws IOException {
 
     try {
