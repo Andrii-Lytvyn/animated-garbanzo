cc07a2a2093c058f9a5f1a7885da209464261161
AlexandrStopkin <stnalex1@gmail.com>
2023-03-30 17:56:21 +0200

    Add  TaskByFinishDateComparator  TaskByS  DateComparator

--- src/TaskByFinishDateComparator.java
+++ src/TaskByFinishDateComparator.java
@@ -1,10 +1,27 @@
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Date;
 
 public class TaskByFinishDateComparator implements Comparator<Task> {
+  public static DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 
   @Override
   public int compare(Task task1, Task task2) {
-    return 0;
+    try {
+      Date date1 = formatter.parse(task1.getFinishTime());
+      Date date2 = formatter.parse(task2.getFinishTime());
+      if (date1.before(date2)) {
+        return -1;
+      } else if (date2.before(date1)) {
+        return 1;
+      }
+      return 0;
+    } catch (ParseException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
+
--- src/TaskByStartDateComparator.java
+++ src/TaskByStartDateComparator.java
@@ -1,11 +1,26 @@
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Comparator;
+import java.util.Date;
 
 public class TaskByStartDateComparator implements Comparator<Task> {
+  public static DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 
   @Override
   public int compare(Task task1, Task task2) {
-
-    return 0;
+    try {
+      Date date1 = formatter.parse(task1.getStartTime());
+      Date date2 = formatter.parse(task2.getStartTime());
+      if (date1.before(date2)) {
+        return -1;
+      } else if (date2.before(date1)) {
+        return 1;
+      }
+      return 0;
+    } catch (ParseException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
